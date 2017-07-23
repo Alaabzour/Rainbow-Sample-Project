9b435fc49e12b8d81dcacb6bfa72ef41901f9341
Alaa <bzouralaa@gmail.com>
2017-07-23 13:54:38 +0300

    Initial Commit

---
+++ RainbowSampleProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RainbowSampleProject
+//
+//  Created by Asal Tech on 7/23/17.
+//  Copyright © 2017 Asaltech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
